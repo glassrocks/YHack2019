707aae2e8d86b43b48117dccfc774c394f03757b
Jared Simmons <simmons.jared76@gmail.com>
2019-10-26 14:06:12 -0400

    Added some extra template code in GoogleCloudThing

--- GoogleCloudThing/main.py
+++ GoogleCloudThing/main.py
@@ -1,9 +1,15 @@
-from flask import Flask
+from google.cloud import vision
+import instaloader
+from flask import Flask, render_template, redirect, request, flash
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+@app.route('/api/instasearch', methods=['POST'])
+def findandshow():
+    pass #replace with code to scrape insta page, and find and display logos
 
 if __name__ == '__main__':
     app.run()
