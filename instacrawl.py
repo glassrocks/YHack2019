8ab019430d49b1a308fa60f217d3034bb0188146
Ian Costa <sammythebeast18@gmail.com>
2019-10-26 17:32:59 -0400

    Added direct messager to instacrawl.py

--- instacrawl.py
+++ instacrawl.py
@@ -1,5 +1,6 @@
 """ Downloads all pictures from an instagram account """
 import instaloader
+from InstagramAPI import InstagramAPI
 
 def find_posts(username):
     """ Returns an Instagram users posts """
@@ -14,9 +15,18 @@ def find_posts(username):
     # Return the profiles posts
     return profile.get_posts()
 
+def message_users(user_ids: list, message):
+    I_API = InstagramAPI("its_ian_costa", "1qaz2wsx")
+    I_API.login()  # login
+    I_API.direct_message(message, user_ids)
+
 
 if __name__ == "__main__":
-    posts = find_posts("ian_costa18")
+    posts = find_posts("donniefinn")
+    id = None
 
     for post in posts:
         print(post.url)
+        id = post.owner_id
+
+    message_users([id], "Hey Donnie what's up")
