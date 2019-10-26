c70aeda77a8a1fc2a9e44e339d3dc3bcbdf9922d
glassrocks <glassrocks@protonmail.com>
2019-10-26 01:49:14 -0400

    added base image detection

--- main.py
+++ main.py
@@ -1,8 +1,34 @@
 """ Main python file for our project """
+import io
+import os
+
+# Imports the Google Cloud client library
+from google.cloud import vision
+from google.cloud.vision import types
+
 
 def main():
-    print("Hello World")
+    # Instantiates a client
+    client = vision.ImageAnnotatorClient()
 
+    # The name of the image file to annotate
+    file_name = os.path.abspath('/home/glassrocks/Pictures/picture_2019-10-26_01-48-00.jpg')
+
+    # Loads the image into memory
+    with io.open(file_name, 'rb') as image_file:
+        content = image_file.read()
+
+    image = types.Image(content=content)
+
+    # Performs label detection on the image file
+    #response = client.label_detection(image=image)
+    #labels = response.label_annotations
+    response = client.logo_detection(image=image)
+    labels = response.logo_annotations
+
+    print('Labels:')
+    for label in labels:
+        print(label.description)    
 
 if __name__ == "__main__":
     main()
